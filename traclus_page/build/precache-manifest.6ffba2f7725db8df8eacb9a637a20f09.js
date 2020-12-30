cd6e52a03835c760fbed070467d7c34d05d3ab06
Iseeblue <rend42@163.com>
2020-12-30 21:41:42 +0800

    upload build folder

---
+++ traclus_page/build/precache-manifest.6ffba2f7725db8df8eacb9a637a20f09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2494ae962e3876ece141a5c7791cd69",
+    "url": "./index.html"
+  },
+  {
+    "revision": "440553224adf72186f25",
+    "url": "./static/css/2.8214dc17.chunk.css"
+  },
+  {
+    "revision": "a22e7f160502ecc34181",
+    "url": "./static/css/main.b761fade.chunk.css"
+  },
+  {
+    "revision": "440553224adf72186f25",
+    "url": "./static/js/2.00626d66.chunk.js"
+  },
+  {
+    "revision": "77ccaffd16cd0eb4657e5506cc1cbbf7",
+    "url": "./static/js/2.00626d66.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a22e7f160502ecc34181",
+    "url": "./static/js/main.0a9e409d.chunk.js"
+  },
+  {
+    "revision": "2cff9fbd3d908c4962b3",
+    "url": "./static/js/runtime-main.e4c4e113.js"
+  }
+]);
